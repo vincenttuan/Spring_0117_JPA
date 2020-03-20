2acbf6119f1d9aa1e90fd99e78b894c06b8853c4
teacher <teacher@192.168.1.180>
2020-03-20 18:26:26 +0800

    Spring JPA

--- src/main/java/com/jpa/one2many/books/entity/Book.java
+++ src/main/java/com/jpa/one2many/books/entity/Book.java
@@ -1,7 +1,6 @@
 package com.jpa.one2many.books.entity;
 
 import java.io.Serializable;
-import java.util.Objects;
 import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;

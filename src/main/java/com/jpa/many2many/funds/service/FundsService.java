5be36736eed67cd3a6e5c6a194f3b08eb21745cd
teacher <teacher@192.168.1.180>
2020-03-23 19:20:09 +0800

    Spring JPA

--- src/main/java/com/jpa/many2many/funds/service/FundsService.java
+++ src/main/java/com/jpa/many2many/funds/service/FundsService.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.jpa.many2many.funds.service;
 
-/**
- *
- * @author teacher
- */
+import org.springframework.stereotype.Service;
+
+@Service("fundsService")
 public class FundsService {
     
 }
